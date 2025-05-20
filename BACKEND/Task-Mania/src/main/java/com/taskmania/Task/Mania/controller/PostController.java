54b990df60aff9d21ab51912ae227d8d88f55636
hiruni500 <hiruniimalsha12@gmail.com>
2025-05-20 10:46:03 +0530

    Update PostController.java

--- BACKEND/Task-Mania/src/main/java/com/taskmania/Task/Mania/controller/PostController.java
+++ BACKEND/Task-Mania/src/main/java/com/taskmania/Task/Mania/controller/PostController.java
@@ -51,7 +51,13 @@ public class PostController {
         return new ResponseEntity<>(post, HttpStatus.OK);
     }
 
-    
+    // Get posts by user ID
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<List<Post>> getPostsByUserId(@PathVariable String userId) {
+        List<Post> posts = postService.getPostsByUserId(userId);
+        return new ResponseEntity<>(posts, HttpStatus.OK);
+    }
+
 
 
 
