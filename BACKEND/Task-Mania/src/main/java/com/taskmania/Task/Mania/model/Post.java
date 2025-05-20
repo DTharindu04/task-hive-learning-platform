efd25ac31d4738ee7a0d705fab3a44f97e3346a4
hiruni500 <hiruniimalsha12@gmail.com>
2025-05-20 09:44:54 +0530

    Post.java

--- BACKEND/Task-Mania/src/main/java/com/taskmania/Task/Mania/model/Post.java
+++ BACKEND/Task-Mania/src/main/java/com/taskmania/Task/Mania/model/Post.java
@@ -1,43 +1,98 @@
-package com.taskmania.Task.Mania.model;
+package com.example.Backend.model;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.mongodb.core.index.Indexed;
-import org.springframework.data.mongodb.core.mapping.Document;
-
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 @Document(collection = "posts")
-@Data
+@AllArgsConstructor
 @NoArgsConstructor
 public class Post {
     @Id
     private String id;
-
-    @Indexed
     private String userId;
-
-    private String title;
+    private String userName;
     private String description;
-
-    private List<String> mediaUrls = new ArrayList<>();
-    private MediaType mediaType;
-
-    private List<String> likedBy = new ArrayList<>();
-    private String skillCategory;
-
-    @CreatedDate
+    private List<String> mediaUrls;
     private Date createdAt;
-
-    @LastModifiedDate
     private Date updatedAt;
+    private List<Like> likes;
+    private List<Comment> comments;
 
-    public enum MediaType {
-        PHOTO,VIDEO
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<String> getMediaUrls() {
+        return mediaUrls;
+    }
+
+    public void setMediaUrls(List<String> mediaUrls) {
+        this.mediaUrls = mediaUrls;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(java.util.Date updatedAt) {
+        this.updatedAt = (Date) updatedAt;
+    }
+
+    public List<Like> getLikes() {
+        return likes;
+    }
+
+    public void setLikes(List<Like> likes) {
+        this.likes = likes;
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
     }
 }
+
